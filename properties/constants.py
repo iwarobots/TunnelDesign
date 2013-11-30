ea59f65748da15ef2ac13a2ae325da86844f4335
Cen Wang <iwarobots@gmail.com>
2013-12-01 00:36:33 +0800

    Add future absolute import and division

--- properties/constants.py
+++ properties/constants.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
 
+from __future__ import absolute_import, division
+
+
 # Heat capacity ratio for air.
 GAMMA = 1.4
--- properties/tests/test_prandtl_meyer_function.py
+++ properties/tests/test_prandtl_meyer_function.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 
+from __future__ import absolute_import, division
+
 import nose
 import nose.tools as nt
 
