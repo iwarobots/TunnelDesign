871d335661912e443725df6827e6e15b53efde95
Cen Wang <iwarobots@gmail.com>
2013-12-05 00:29:26 +0800

    Add tests for inverse_prandtl_meyer_function

--- properties/tests/test_prandtl_meyer_function.py
+++ properties/tests/test_prandtl_meyer_function.py
@@ -6,21 +6,32 @@ from __future__ import absolute_import, division
 import nose
 import nose.tools as nt
 
-from properties.prandtl_meyer_function import nu_in_deg
+from properties.prandtl_meyer_function import (m2nu_in_deg,
+                                               nu_in_deg2m)
 
 
 @nt.raises(ValueError)
 def test_mach_lesser_than_one():
     m = 0.1
-    nu_in_deg(m)
+    m2nu_in_deg(m)
 
 
 def test_normal_mach():
     m1 = 1.5
-    nt.assert_almost_equal(nu_in_deg(m1), 11.9052, places=4)
+    nt.assert_almost_equal(m2nu_in_deg(m1), 11.9052, places=4)
 
     m2 = 2.6
-    nt.assert_almost_equal(nu_in_deg(m2), 41.4147, places=4)
+    nt.assert_almost_equal(m2nu_in_deg(m2), 41.4147, places=4)
+
+
+@nt.raises(ValueError)
+def test_inverse_prandtl_meyer_function_when_nu_is_less_than_zero():
+    nu = -1
+    nu_in_deg2m(nu)
+
+def test_inverse_prandtl_meyer_function():
+    nu1 = 11.9052
+    nt.assert_almost_equal(nu_in_deg2m(nu1), 1.5, places=1)
 
 
 if __name__ == '__main__':
