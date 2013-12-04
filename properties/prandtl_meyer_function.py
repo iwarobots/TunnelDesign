079966335015ae373b4f63a0f1a4e2bb3786b332
Cen Wang <iwarobots@gmail.com>
2013-12-05 00:30:47 +0800

    Add inverse_prandtl_meyer_function, rename prandtl_meyer_function

--- properties/prandtl_meyer_function.py
+++ properties/prandtl_meyer_function.py
@@ -3,12 +3,18 @@
 
 from __future__ import absolute_import, division
 
-from math import asin, atan, degrees, sqrt
+from math import asin, atan, degrees, radians, sqrt
+
+from scipy.optimize import brentq
 
 from properties.constants import GAMMA
 
 
-def nu_in_rad(m):
+MIN_MACH = 1E-5
+MAX_MACH = 1E1
+
+
+def m2nu_in_rad(m):
     if m < 1:
         raise ValueError('Mach number should be greater than or equal to 1')
     a = (GAMMA+1) / (GAMMA-1)
@@ -17,8 +23,19 @@ def nu_in_rad(m):
     return sqrt(a) * atan(sqrt(c)) - atan(sqrt(b))
 
 
-def nu_in_deg(m):
-    return degrees(nu_in_rad(m))
+def m2nu_in_deg(m):
+    return degrees(m2nu_in_rad(m))
+
+
+def nu_in_rad2m(nu):
+    if nu < 0:
+        raise ValueError('nu should be greater than or equal to 0')
+    return brentq(lambda x: m2nu_in_rad(x)-nu, 1, MAX_MACH)
+
+
+def nu_in_deg2m(nu):
+    nu = radians(nu)
+    return nu_in_rad2m(nu)
 
 
 def mu_in_rad(m):
