0425dff683a2bbb204785b6617ad074428c5e6a6
Cen Wang <iwarobots@gmail.com>
2013-12-06 02:19:19 +0800

    Rename Mach angle function

--- properties/tests/test_mach_angle.py
+++ properties/tests/test_mach_angle.py
@@ -8,21 +8,21 @@ from __future__ import absolute_import, division
 import nose
 import nose.tools as nt
 
-from properties.prandtl_meyer_function import mu_in_deg
+from properties.prandtl_meyer_function import m2mu_in_deg
 
 
 @nt.raises(ValueError)
 def test_mach_lesser_than_one():
     m = 0.1
-    mu_in_deg(m)
+    m2mu_in_deg(m)
 
 
 def test_normal_mach():
     m1 = 1.5
-    nt.assert_almost_equal(mu_in_deg(m1),  41.8103148, places=3)
+    nt.assert_almost_equal(m2mu_in_deg(m1),  41.8103148, places=3)
 
     m2 = 2.6
-    nt.assert_almost_equal(mu_in_deg(m2),  22.6198649, places=3)
+    nt.assert_almost_equal(m2mu_in_deg(m2),  22.6198649, places=3)
 
 
 if __name__ == '__main__':
